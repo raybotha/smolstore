29c4e12530d47140ac98568f48c977e4cc4ed859
Raymond Botha <botha@riseup.net>
2019-08-06 16:36:31 +0200

    Bump to v0.2.0a0

--- smolstore/__init__.py
+++ smolstore/__init__.py
@@ -3,5 +3,5 @@ from .exceptions import UniqueViolation
 from .field import Field
 from .store import SmolStore
 
-__version__ = "0.1.1a"
+__version__ = "0.2.0a"
 __all__ = (SmolStore, Field, UniqueViolation)
